cd01563334e263602fbf73954189663b937d5283
Sreevathsa Golla <sreevathsagolla24@gmail.com>
2024-05-23 16:26:02 +0100

    Made it more generic. Added argparse functionality and ability to plot SST and SSTAs.

--- interactive_plot_maker.py
+++ interactive_plot_maker.py
@@ -7,8 +7,15 @@ from mplcursors import cursor
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Interactive plot for mean global SST anomaly (calculated w.r.t previous year temperatures).")
+    parser = argparse.ArgumentParser(description="Interactive plot for mean global SST/SSTA.")
 
+
+    parser.add_argument(
+        '--data_type',
+        type=str,
+        required=True,
+        help="Type of data: SST, SSTA_1 [w.r.t. first 30yr baseline], SSTA_2 [w.r.t. previous year temperatures i.e. SST_y - SST_(y-1)]"
+    )
     parser.add_argument(
         '--obs_name',
         type=str,
@@ -20,7 +27,7 @@ def main():
         '--start_year',
         type=int,
         required=True,
-        help="Start year: Any year >= 1983 (for NOAA_OISST) and >= 1871 (for HadISST)"
+        help="Start year: Any year >= 1982 (for NOAA_OISST) and >= 1870 (for HadISST)"
     )
     parser.add_argument(
         '--end_year',
@@ -28,12 +35,22 @@ def main():
         required=True,
         help="End year: Any year <= 2023"
     )
+    parser.add_argument(
+        '--baseline',
+        type=str,
+        help="Baseline in the format of 'YYYY-YYYY'"
+    )
 
     args = parser.parse_args()
 
-    start_year, end_year, obs_name = args.start_year, args.end_year, args.obs_name
+    data_type, start_year, end_year, obs_name = args.data_type, args.start_year, args.end_year, args.obs_name
 
-    # Validate years
+    if data_type == 'SSTA_1':
+        if args.baseline:
+            validate_baseline(args.baseline)
+        else:
+            raise argparse.ArgumentTypeError("baseline invalid or not given, please retry.")
+
     validate_years(start_year, end_year, obs_name)
 
     if obs_name == 'HadISST':
@@ -44,16 +61,36 @@ def main():
         year_range = pd.date_range(start='01-01-2023', end='31-12-2023', freq='D')
 
 
-    df = pd.read_csv(obs_name+'_globmean_sst_anom_wrt_prev_yr.csv')
+    df = pd.read_csv('./data/'+obs_name+'_globmean_sst_data.csv')
     df['TIME'] = pd.to_datetime(df['TIME'])
     df = df.set_index('TIME')
     df = df[~((df.index.month == 2) & (df.index.day == 29))]
     df = df[(df.index.year>=start_year) & (df.index.year<=end_year)]
-
+    
+    if data_type == 'SSTA_1':
+        bs_start, bs_end = [int(x) for x in args.baseline.split('-')]
+        bs_df = df[(df.index.year >= bs_start) & (df.index.year <= bs_end)]
+        climatology = bs_df.groupby([bs_df.index.month, bs_df.index.day]).mean()
+        def get_climatology(row, climatology):
+            month, day = row.name.month, row.name.day
+            return climatology.loc[(month, day)]
+        df['Climatology'] = df.apply(get_climatology, axis=1, climatology=climatology)
+        df['SST'] = df['SST'] - df['Climatology']
+        df.drop(columns='Climatology', inplace=True)
+        dtype_title = 'SSTA (Baseline: '+args.baseline+')'
+    elif data_type == 'SSTA_2':
+        anom = df.copy(deep = True)
+        start_year = start_year + 1
+        for year in range(start_year,end_year+1):
+            anom[anom.index.year == year] = df[df.index.year == year].values - df[df.index.year == (year-1)].values
+        df = anom[(anom.index.year>=start_year) & (anom.index.year<=end_year)]
+        dtype_title = 'SSTA (w.r.t. previous year mean SST)'
+    elif data_type == 'SST':
+        dtype_title = 'SST'
 
     fig, ax = plt.subplots()
     fig.subplots_adjust(bottom=0.2)
-    ax.set_title(obs_name+' | Mean SSTA (w.r.t. prev. year) | World 60S-60N | '+str(start_year)+'-'+str(end_year),
+    ax.set_title(obs_name+' | Mean '+dtype_title+' | World 60S-60N | '+str(start_year)+'-'+str(end_year),
                     fontweight='bold',
                     fontsize=16)
     ax.grid(True)
@@ -87,16 +124,23 @@ def main():
 
 
 def validate_years(start_year, end_year, obs_name):
-    if obs_name == 'NOAA_OISST' and start_year < 1983:
-        raise argparse.ArgumentTypeError("start_year must be >= 1983 for NOAA_OISST")
-    if obs_name == 'HadISST' and start_year < 1871:
-        raise argparse.ArgumentTypeError("start_year must be >= 1871 for HadISST")
+    if obs_name == 'NOAA_OISST' and start_year < 1982:
+        raise argparse.ArgumentTypeError("start_year must be >= 1982 for NOAA_OISST")
+    if obs_name == 'HadISST' and start_year < 1870:
+        raise argparse.ArgumentTypeError("start_year must be >= 1870 for HadISST")
     if end_year > 2023:
         raise argparse.ArgumentTypeError("end_year must be <= 2023")
     if start_year > end_year:
         raise argparse.ArgumentTypeError("start_year must be less than or equal to end_year")
     return start_year, end_year
 
+def validate_baseline(baseline):
+    try:
+        start_baseline, end_baseline = map(int, baseline.split('-'))
+        if start_baseline > end_baseline:
+            raise argparse.ArgumentTypeError("Baseline start year must be less than or equal to baseline end year")
+    except ValueError:
+        raise argparse.ArgumentTypeError("Baseline must be in the format 'YYYY-YYYY'")
 
 def interactive_legend(ax=None):
     if ax is None:
